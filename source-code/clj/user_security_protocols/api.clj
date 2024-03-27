dc2673647b97e2d81bba95089e8ebaf36c157fda
Bit Handshake <adam@monotech.hu>
2024-03-27 23:10:27 +0100

    0.1.1.2

--- source-code/clj/user_security_protocols/api.clj
+++ source-code/clj/user_security_protocols/api.clj
@@ -4,7 +4,7 @@
 
 ;; ----------------------------------------------------------------------------
 ;; ----------------------------------------------------------------------------
-
+ 
 ; @redirect (user-security-protocols.protocols/*)
 (def check-user-identifier                protocols/check-user-identifier)
 (def create-user-account                  protocols/create-user-account)
